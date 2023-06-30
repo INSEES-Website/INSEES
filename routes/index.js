78df0d9e35cf846fd45b542dd99dbe7b040118e8
Rupantar <rupantarborphukan@gmail.com>
2023-06-30 16:16:11 +0530

    Loading page done

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,21 @@
-const express = require('express')
+const express = require("express");
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    // res.send('hello!! this is express from node js')
-    const params = { 'title': 'home' }
-    res.render('index', params);
+router.get("/", (req, res) => {
+  // res.send('hello!! this is express from node js')
+  const params = { title: "home" };
+  res.render("index", params);
 });
 
-router.get('/our_activities', (req, res) => {
-    // res.send('hello!! this is express from node js')
-    const params = { 'title': 'our_activities' }
-    res.render('our_activities', params)
+router.get("/our_activities", (req, res) => {
+  // res.send('hello!! this is express from node js')
+  const params = { title: "our_activities" };
+  res.render("our_activities", params);
 });
 
+router.get("/loading", (req, res) => {
+  const params = { title: "loading" };
+  res.render("loading", params);
+});
 
 module.exports = router;
